f43c353fc086e18754aa862bb7d1a0a44686c025
Sam Thompson <samthompson292@gmail.com>
2021-03-18 15:40:00 +0000

    can now join a room

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,14 +1,6 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import TextField from "@material-ui/core/TextField";
-import FormHelperText from "@material-ui/core/FormHelperText";
-import FormControl from "@material-ui/core/FormControl";
 import { Link } from "react-router-dom";
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { FormControlLabel, RadioGroup, Radio, FormControl, FormHelperText, TextField, Typography, Grid, Button } from "@material-ui/core";
 
 export default class CreateRoomPage extends Component {
   defaultVotes = 2;
