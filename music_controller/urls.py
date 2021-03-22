57701408b8a67df872f4a8dfb1bf56619bcd1b5f
Sam Thompson <samthompson292@gmail.com>
2021-03-22 16:19:37 +0000

    use templateview to see react app

--- music_controller/urls.py
+++ music_controller/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('spotify/', include('spotify.urls')),
-    path('', include('frontend.urls')),
-    path('main', include('main.urls')),
+    path('', TemplateView.as_view(template_name='frontend/index.html')),
+    path('main/', include('main.urls')),
 ]
